5f4f6af3ff45c4f9b9539830c51f9c51f64830a0
galibremo <galibremo@gmail.com>
2023-10-11 22:10:08 +0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 const port = 3000;
 
-app.get((req,res)=>{
-    res.render();
+app.get((req, res) => {
+  res.render();
 });
 
-app.listen(port,()=>{
-    console.log("listening from port: " + port);
+app.listen(port, () => {
+  console.log("listening from port: " + port);
 });
