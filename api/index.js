b8ae10f353356ee0862754841d4023d24b570d73
galibremo <galibremo@gmail.com>
2023-10-12 21:02:34 +0600

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
+import 'dotenv/config';
 import express from "express";
 import mongoose from "mongoose";
-import 'dotenv/config';
+import userRouter from "./routes/user.route.js";
+
 
 mongoose
   .connect(process.env.MONGO)
@@ -11,13 +13,11 @@ mongoose
     console.log(err);
   });
 
-  
+
 const app = express();
 const port = 3000;
 
-app.get((req, res) => {
-  res.render();
-});
+app.use("/api/user",userRouter);
 
 app.listen(port, () => {
   console.log("listening from port: " + port);
