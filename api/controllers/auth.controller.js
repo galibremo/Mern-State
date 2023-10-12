f98660def00764e52ea0d79e0011fcb624014988
galibremo <galibremo@gmail.com>
2023-10-12 22:00:03 +0600

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+
+export const signup = async(req,res)=>{
+    console.log(req.body);
+    const {username,email,password} = req.body;
+    const newUser = new User({username,email,password});
+    try
+    {
+        await newUser.save();
+        res.status(201).json("user create successfully!");
+    }catch(error)
+    {
+        res.status(500).json(error.message);
+    }
+    
+};
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
-
+import authRouter from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,8 +16,10 @@ mongoose
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
 
 app.listen(port, () => {
   console.log("listening from port: " + port);
