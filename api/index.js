413538e53ed8f8b8ff9e9156515a15800365343a
galibremo <galibremo@gmail.com>
2023-10-27 23:41:26 +0600

    create update user api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from 'cookie-parser';
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,6 +18,7 @@ mongoose
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
