028b6680d8b9968307cb68a907769ef74e7f6e63
galibremo <galibremo@gmail.com>
2023-11-02 23:39:39 +0600

    complete create listing page funtionality

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -31,7 +31,7 @@ const listingSchema = new mongoose.Schema(
       required: true,
     },
     furnished: {
-      type: Number,
+      type: Boolean,
       required: true,
     },
     parking: {
